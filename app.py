f153cffffb14978e282adb633a6f2e645199b9c7
Om Maniya <105499099+maniyaom@users.noreply.github.com>
2024-10-11 13:07:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/ip', methods=['GET'])
-def ip():
+@app.route('/', methods=['GET'])
+def home():
     return jsonify({
-            "error": "false",
-            "message": request.remote_addr
-        })
+        "error": "false",
+        "message": "Home Page"
+    })
+
+@app.route('/ip', methods=['GET'])
+def get_ip():
+    return jsonify({
+        "error": "false",
+        "message": request.remote_addr
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
