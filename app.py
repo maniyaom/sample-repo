06d942659a6b5fd88e08abbd3091d60e1c1c9ac5
Om Maniya <105499099+maniyaom@users.noreply.github.com>
2024-10-12 17:02:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ def home():
 
 @app.route('/ip', methods=['GET'])
 def get_ip():
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
     return jsonify({
         "error": "false",
-        "message": request.remote_addr
+        "message": ip
     })
 
 if __name__ == '__main__':
